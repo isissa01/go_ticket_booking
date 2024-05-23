5fdf95d5332e69afeeb319d9179fbd4ff86d2f56
Issa <isissa01@gmail.com>
2024-05-23 10:06:33 -0400

    refactored code into seperate functions

--- main.go
+++ main.go
@@ -9,18 +9,17 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
-	fmt.Println("Welcome to", conferenceName, "booking system")
-	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
 
 	var bookings []string
 
+	greetUsers(conferenceName, conferenceTickets, remainingTickets)
+
 	//ask user for their name
 	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
 		//ask user for their first name
 		fmt.Print("Enter your first name: ")
@@ -54,38 +53,44 @@ func main() {
 			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 			fmt.Printf("%v tickets remaining for the %v\n", remainingTickets, conferenceName)
 
-			firstNames := []string{}
-			for _, booking := range bookings {
-				firstNames = append(firstNames, strings.Split(booking, " ")[0])
-			}
-
-			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+			printFirstNames(bookings)
 
 			if remainingTickets == 0 {
 				fmt.Println("Our conference is booked out. Come back next year.")
 				break
 			}
 		} else {
-			if !isValidName {
-				fmt.Println("Name must be at least 2 characters")
-
-			}
-			if !isValidEmail {
-				fmt.Println("Email address is invalid")
-
-			}
-			if !isValidTicketNumber {
-				fmt.Println("Number of tickets is invalid")
-
-			}
+			validateUserData(isValidName, isValidEmail, isValidTicketNumber)
 			//validate user input
 		}
 	}
 
-	//send ticket to user email
+}
+func greetUsers(conferenceName string, conferenceTickets uint, remainingTickets uint) {
+	fmt.Println("Welcome to", conferenceName, "booking system")
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+}
+func validateUserData(isValidName bool, isValidEmail bool, isValidTicketNumber bool) {
+	if !isValidName {
+		fmt.Println("Name must be at least 2 characters")
 
-	//call function to print out summary
+	}
+	if !isValidEmail {
+		fmt.Println("Email address is invalid")
 
-	//call function to send ticket to email
+	}
+	if !isValidTicketNumber {
+		fmt.Println("Number of tickets is invalid")
+	}
+}
+
+func printFirstNames(bookings []string) {
+	firstNames := []string{}
+	for _, booking := range bookings {
+		firstNames = append(firstNames, strings.Split(booking, " ")[0])
+	}
+
+	fmt.Printf("The first names of bookings are: %v\n", firstNames)
 
 }
